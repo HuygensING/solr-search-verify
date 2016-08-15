d8d7dd12bda1bc622e4e9e7c86d84a5ab3bd56a2
renevanderark <r.van.der.ark@gmail.com>
2016-08-15 11:23:58 +0200

    adds local solr conf for proxy

--- scripts/server.js
+++ scripts/server.js
@@ -21,14 +21,23 @@ function onFilesChanged(event, file) {
 
 browserSync.watch(watchFiles, debounce(onFilesChanged, 300));
 
-var proxyOptions = url.parse("http://10.152.32.51:8983/solr");
-proxyOptions.route = "/solr";
+var localSolr = process.env.SOLR || "http://localhost:8983/solr";
+var wwdocumentsOptions = url.parse(localSolr + "/wwdocuments/select");
+wwdocumentsOptions .route = "/repositorysolr/wwdocuments";
+
+var wwpersonsOptions = url.parse(localSolr + "/wwpersons/select");
+wwpersonsOptions.route = "/repositorysolr/wwpersons";
+
+var wwdocumentreceptionsOptions = url.parse(localSolr + "/wwdocumentreceptions/select");
+wwdocumentreceptionsOptions.route = "/repositorysolr/wwdocumentreceptions";
 
 browserSync.init({
 	server: {
 		baseDir: baseDir,
 		middleware: [
-			proxy(proxyOptions),
+			proxy(wwdocumentsOptions),
+			proxy(wwpersonsOptions),
+			proxy(wwdocumentreceptionsOptions),
 			modRewrite([
 				"^/css/(.*)$ /css/$1 [L]",
 				"^/js/(.*)$ /js/$1 [L]",
