1372b44514944d711c962f26190d0a9c13952abe
renevanderark <r.van.der.ark@gmail.com>
2016-08-22 15:16:41 +0200

    replaces onClick with Link to

--- src/components/search/custom/persons.js
+++ src/components/search/custom/persons.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { defaultComponentPack } from "solr-faceted-search-react";
+import { urls } from "../../../router";
+import { Link } from "react-router";
 
 const personComponents = {
 	...defaultComponentPack,
@@ -7,7 +9,7 @@ const personComponents = {
 		...defaultComponentPack.results,
 		result: (props) => (
 			<li className="list-group-item">
-				<a onClick={() => props.onSelect(props.doc.id)} style={{display: "flex"}}>
+				<Link to={urls.authorIndex(props.doc.id)} style={{display: "flex"}}>
 					<span style={{"flexGrow": "1", whiteSpace: "nowrap", paddingRight: "1em"}}>{props.doc.displayName_s}</span>
 					{props.doc.birthDate_i || props.doc.deathDate_i
 						? <span style={{color: "#666", whiteSpace: "nowrap", paddingRight: "1em"}}>
@@ -19,7 +21,7 @@ const personComponents = {
 					<span title={props.doc.relatedLocations_ss ? props.doc.relatedLocations_ss.join(", ") : null} style={{"flexShrink": 1, color: "#aaa", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
 						{props.doc.relatedLocations_ss ? props.doc.relatedLocations_ss.join(", ") : null}
 					</span>
-				</a>
+				</Link>
 			</li>
 		)
 	}
