8bcbef23b856a9498372953101b43c492a4f67d2
renevanderark <r.van.der.ark@gmail.com>
2016-08-23 11:49:30 +0200

    adds overview labels for inbound and outbound receptions on documents.

--- src/definitions/publication-receptions.js
+++ src/definitions/publication-receptions.js
@@ -17,7 +17,75 @@ export default {
 		"listsWork",
 		"mentionsWork",
 		"isParodyOf",
-		"quotesWork",
+		"quotesWork"
+/*
 		"referencesWork"
-	]
+*/
+	],
+	outBound: [
+		"hasEdition",
+		"hasSequel",
+		"hasTranslation",
+		"hasAdaptation",
+		"hasPlagiarismBy",
+		"isAnnotatedIn",
+		"hasBibliography",
+		"isCensoredBy",
+		"isWorkCommentedOnIn",
+		"containedInAnthology",
+		"isCopiedBy",
+		"isWorkAwarded",
+		"hasPreface",
+		"isIntertextualOf",
+		"isWorkListedOn",
+		"isWorkMentionedIn",
+		"isParodiedBy",
+		"isWorkQuotedIn"
+/*
+		"isWorkReferencedIn"
+*/
+	],
+	overviewLabels: {
+		"hasEdition": "Was issued as",
+		"hasSequel": "Was continued as",
+		"hasTranslation": "Was translated as",
+		"hasAdaptation": "Was adapted as",
+		"hasPlagiarismBy": "Was plagiarized in",
+		"isAnnotatedIn": "Was annotated in",
+		"hasBibliography": "Was mentioned in BBC",
+		"isCensoredBy": "Was prohibited through",
+		"isWorkCommentedOnIn": "Was commented on in",
+		"containedInAnthology": "Was included in",
+		"isCopiedBy": "Was reissued as",
+		"isWorkAwarded": "Was awarded through",
+		"hasPreface": "Was prefaced in",
+		"isIntertextualOf": "Was intertextually used in",
+		"isWorkListedOn": "Was listed in",
+		"isWorkMentionedIn": "Was mentioned in",
+		"isParodiedBy": "Was parodied in",
+		"isWorkQuotedIn": "Was quoted in",
+
+		"isEditionOf": "Is an edition of",
+		"isSequelOf": "Is a sequel of",
+		"isTranslationOf": "Is a translation of",
+		"isAdaptationOf": "Is an adaptation of",
+		"isPlagiarismOf": "Plagiarizes",
+		"hasAnnotationsOn": "Provides handwritten notes about",
+		"isBibliographyOf": "BBC mentions (briefly)",
+		"isCensoringOf": "Prohibits",
+		"commentsOnWork": "Is a comment on / upon",
+		"isAnthologyContaining": "Contains",
+		"isCopyOf": "Is another edition of",
+		"isAwardForWork": "Is awarding",
+		"isPrefaceOf": "Prefaces",
+		"isIntertextualTo": "Refers intertextually to",
+		"listsWork": "Lists",
+		"mentionsWork": "Mentions (briefly)",
+		"isParodyOf": "Parodies",
+		"quotesWork": "Quotes"
+/*
+		"referencesWork"
+		"isWorkReferencedIn"
+*/
+	}
 };
