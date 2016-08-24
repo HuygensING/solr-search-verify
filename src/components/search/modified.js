cd4c15ea316a6984d3a190538ac80f52ff4581e1
renevanderark <r.van.der.ark@gmail.com>
2016-08-24 12:12:18 +0200

    adds modified search for publications.

--- src/components/search/modified.js
+++ src/components/search/modified.js
@@ -2,6 +2,7 @@ import React from "react";
 import SolrFacetedSearch from "solr-faceted-search-react";
 import CustomRangeFacet from "./custom/range-facet/modified";
 import personModifiedClient from "../../search-clients/person-modified-client";
+import documentModifiedClient from "../../search-clients/document-modified-search";
 import { defaultComponentPack } from "solr-faceted-search-react";
 import { makeLabel, makeDate } from "../../util/date";
 import { Link } from "react-router";
@@ -38,12 +39,32 @@ const authorCustomComponents = {
 		...customComponents.results,
 		result: (props) => {
 			return (
-				<li className="list-group-item">
-					<Link to={urls.authorIndex(props.doc.id)}>
+				<li className="list-group-item" style={{display: "flex"}}>
+					<Link to={urls.authorIndex(props.doc.id)} style={{"flexGrow": "1", whiteSpace: "nowrap", paddingRight: "1em", maxWidth: "80%", overflow: "hidden", textOverflow: "ellipsis"}} >
 						{props.doc.displayName_s}
 					</Link>
-					<span className="pull-right">
-								{props.doc.modifiedBy_s}{" "}
+					<span style={{"flexShrink": 1, color: "#aaa", whiteSpace: "nowrap"}}>
+						{props.doc.modifiedBy_s}{" "}
+						({makeLabel(makeDate(new Date(props.doc.modified_l)))})
+					</span>
+				</li>
+			);
+		}
+	}
+};
+
+const publicationCustomComponents = {
+	...customComponents,
+	results: {
+		...customComponents.results,
+		result: (props) => {
+			return (
+				<li className="list-group-item" style={{display: "flex"}}>
+					<Link to={urls.publicationIndex(props.doc.id)} style={{"flexGrow": "1", whiteSpace: "nowrap", paddingRight: "1em", maxWidth: "80%", overflow: "hidden", textOverflow: "ellipsis"}} >
+						{props.doc.displayName_s}
+					</Link>
+					<span style={{"flexShrink": 1, color: "#aaa", whiteSpace: "nowrap"}}>
+						{props.doc.modifiedBy_s}{" "}
 						({makeLabel(makeDate(new Date(props.doc.modified_l)))})
 					</span>
 				</li>
@@ -58,20 +79,32 @@ class ModifiedSearch extends React.Component {
 		const { modifiedSearch: { authors } } = this.props;
 		if (authors.query.searchFields.length === 0) {
 			personModifiedClient.initialize();
+			documentModifiedClient.initialize();
 		}
 	}
 
 	render() {
-		const { modifiedSearch: { authors } } = this.props;
+		const { modifiedSearch: { authors, publications } } = this.props;
 		return (
-			<div className="col-md-6">
-				<SolrFacetedSearch
-					{...authors}
-					{...personModifiedClient.getHandlers()}
-					customComponents={authorCustomComponents}
-					bootstrapCss={true}
-					onSelectDoc={() => {}}
-				/>
+			<div>
+				<div className="col-md-6">
+					<SolrFacetedSearch
+						{...authors}
+						{...personModifiedClient.getHandlers()}
+						customComponents={authorCustomComponents}
+						bootstrapCss={true}
+						onSelectDoc={() => {}}
+					/>
+				</div>
+				<div className="col-md-6">
+					<SolrFacetedSearch
+						{...publications}
+						{...documentModifiedClient.getHandlers()}
+						customComponents={publicationCustomComponents}
+						bootstrapCss={true}
+						onSelectDoc={() => {}}
+					/>
+				</div>
 			</div>
 		);
 	}
--- src/reducers/modified-search.js
+++ src/reducers/modified-search.js
@@ -9,6 +9,17 @@ const initialState = {
 			facets: [],
 			numFound: 0
 		}
+	},
+	publications: {
+		query: {
+			searchFields: [],
+			sortFields: []
+		},
+		results: {
+			docs: [],
+			facets: [],
+			numFound: 0
+		}
 	}
 };
 
@@ -16,6 +27,8 @@ export default function(state=initialState, action) {
 	switch (action.type) {
 		case "SET_PERSON_MODIFIED_SEARCH_STATE":
 			return {...state, authors: action.state};
+		case "SET_DOCUMENT_MODIFIED_SEARCH_STATE":
+			return {...state, publications: action.state};
 	}
 	return state;
 }
