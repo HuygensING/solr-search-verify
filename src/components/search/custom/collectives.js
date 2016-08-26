6f46d63faf6a7dc6587836c54a251bf627e679af
renevanderark <r.van.der.ark@gmail.com>
2016-08-26 15:01:51 +0200

    adds result index number for collectives.

--- src/components/search/custom/collectives.js
+++ src/components/search/custom/collectives.js
@@ -9,6 +9,10 @@ const collectiveComponents = {
 		...defaultComponentPack.results,
 		result: (props) => (
 			<li className="list-group-item">
+					<span style={{textAlign: "right", display: "inline-block", width: "40px", paddingRight: "10px"}}>
+						{props.start / props.rows * props.rows + props.resultIndex + 1}.
+					</span>
+
 				<Link to={urls.collectiveIndex(props.doc.id)}>
 					{props.doc.displayName_s}
 				</Link>
