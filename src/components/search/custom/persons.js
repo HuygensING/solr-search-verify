7076d8e0e0c3668132c1cafaa7acf76987ec100c
renevanderark <r.van.der.ark@gmail.com>
2016-08-26 14:37:27 +0200

    adds result index number for authors.

--- src/components/search/custom/persons.js
+++ src/components/search/custom/persons.js
@@ -2,28 +2,13 @@ import React from "react";
 import { defaultComponentPack } from "solr-faceted-search-react";
 import { urls } from "../../../router";
 import { Link } from "react-router";
+import AuthorResult from "./result/author";
 
 const personComponents = {
 	...defaultComponentPack,
 	results: {
 		...defaultComponentPack.results,
-		result: (props) => (
-			<li className="list-group-item">
-				<Link to={urls.authorIndex(props.doc.id)} style={{display: "flex"}}>
-					<span style={{"flexGrow": "1", whiteSpace: "nowrap", paddingRight: "1em"}}>{props.doc.displayName_s}</span>
-					{props.doc.birthDate_i || props.doc.deathDate_i
-						? <span style={{color: "#666", whiteSpace: "nowrap", paddingRight: "1em"}}>
-								{props.doc.birthDate_i || "?"}
-								{" "}&mdash;{" "}
-								{props.doc.deathDate_i || "?"}
-							</span>
-						: null}
-					<span title={props.doc.relatedLocations_ss ? props.doc.relatedLocations_ss.join(", ") : null} style={{"flexShrink": 1, color: "#aaa", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
-						{props.doc.relatedLocations_ss ? props.doc.relatedLocations_ss.join(", ") : null}
-					</span>
-				</Link>
-			</li>
-		)
+		result: AuthorResult
 	}
 };
 
