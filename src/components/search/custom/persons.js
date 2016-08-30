8385c4797646333d9ff73d09aaa4e74543838646
renevanderark <r.van.der.ark@gmail.com>
2016-08-30 13:01:10 +0200

    unused imports.

--- src/components/search/custom/persons.js
+++ src/components/search/custom/persons.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { defaultComponentPack } from "solr-faceted-search-react";
-import { urls } from "../../../router";
-import { Link } from "react-router";
 import AuthorResult from "./result/author";
 
 const personComponents = {
