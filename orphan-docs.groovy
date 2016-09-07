d0f5d8a7b4d67ac28077d026e4fd4f6fbb9ad063
renevanderark <r.van.der.ark@gmail.com>
2016-09-07 08:10:06 +0200

    removes groovy script.

--- orphan-docs.groovy
+++
@@ -1,9 +0,0 @@
-g.V()
- .hasLabel("collection")
- .has("collectionName", "wwdocuments")
- .out("hasEntityNode")
- .out("hasEntity")
- .where(__.not(out("isCreatedBy")))
- .map {
-   "http://test.repository.huygens.knaw.nl/v2.1/domain/wwdocuments/" + it.get().value("tim_id") + "\r\n"
- }
